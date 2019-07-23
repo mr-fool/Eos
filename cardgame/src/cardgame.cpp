48a37ed4f18e8699b5a3b14bf0b64b145507778d
mr-fool <mr-fool@users.noreply.github.com>
2019-07-23 12:25:53 -0600

    practicing

--- cardgame/src/cardgame.cpp
+++ cardgame/src/cardgame.cpp
@@ -2,6 +2,11 @@
 #include "gameplay.cpp"
 
 ACTION cardgame::hi(name user) {
-  require_auth(user);
+  require_auth(get_self());
   print("Hello, ", name{user});
+}
+ACTION trymessage (name sender, name recipient) {
+    require_auth(sender);
+    print("Sorry, you will have to say hello to ", name{recipient}, " in person");
+ 
 }
