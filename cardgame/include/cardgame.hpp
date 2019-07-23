288d1f6622be1d469c53d6ac6bd92b0317344f00
mr-fool <mr-fool@users.noreply.github.com>
2019-07-23 15:30:36 -0600

    Update cardgame.hpp

--- cardgame/include/cardgame.hpp
+++ cardgame/include/cardgame.hpp
@@ -6,16 +6,27 @@ using namespace eosio;
 CONTRACT cardgame : public contract {
   public:
     using contract::contract;
-    cardgame(eosio::name receiver, eosio::name code, datastream<const char*> ds):contract(receiver, code, ds) {}
+    cardgame(eosio::name receiver, 
+      eosio::name code, datastream<const char*> ds):
+      contract(receiver, code, ds), 
+      _users{receiver, receiver.value}
 
     ACTION hi(name user);
   
   private:
-    TABLE tableStruct {
-      name key;
-      std::string name;
+    TABLE user_info {
+      name username;
+      uint64_t win_count = 0;
+      uint64_t lost_count = 0;
+      
+      auto primary_key() const {
+        return username.value;
+        
+      }
     };
-    typedef eosio::multi_index<"table"_n, tableStruct> table;
+    typedef eosio::multi_index
+    <name("table"), user_info> users_table;
+    users_table _users;
 };
 
 EOSIO_DISPATCH(cardgame, (hi))
