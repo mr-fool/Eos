9662d6e6578b6ba234ec2d5bb4f43742b4ba1979
hetzer <hetzer@hetzers-MacBook-Pro.local>
2019-07-23 18:48:58 -0600

    issue

--- cardgame/include/cardgame.hpp
+++ cardgame/include/cardgame.hpp
@@ -28,6 +28,17 @@ CONTRACT cardgame : public contract {
     typedef eosio::multi_index
     <name("table"), user_info> users_table;
     users_table _users;
+    
+    TABLE game_struct {
+      uint64_t gameid = 0;
+      bool isenabled;
+      std::string gameName;
+        
+    };
+     
+    typedef eosio::multi_index
+    <name("games"), game_struct> game_table;
+    game_table _games;
 };
 
 EOSIO_DISPATCH(cardgame, (hi))
