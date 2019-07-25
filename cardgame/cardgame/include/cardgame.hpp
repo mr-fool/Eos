8228aa3e5d0d2c838b17110f5577409dfd456f4d
mr-fool <mr-fool@users.noreply.github.com>
2019-07-25 17:45:24 -0600

    more changes

--- cardgame/cardgame/include/cardgame.hpp
+++ cardgame/cardgame/include/cardgame.hpp
@@ -9,17 +9,17 @@ CONTRACT cardgame : public contract {
     cardgame(eosio::name receiver, 
       eosio::name code, datastream<const char*> ds):
       contract(receiver, code, ds), 
-      _users(receiver, receiver.value),
-      _games(receiver, receiver.value)
+      _users(receiver, receiver.value)
       {}
 
-    ACTION login(name user);
+    ACTION login(name user, std::string usermessage);
   
   private:
     TABLE user_info {
       name username;
       uint64_t win_count = 0;
       uint64_t lost_count = 0;
+      std::string    usermessage; 
       
       auto primary_key() const {
         return username.value;
@@ -27,19 +27,9 @@ CONTRACT cardgame : public contract {
       }
     };
     typedef eosio::multi_index
-    <name("table"), user_info> users_table;
+    <name("users"), user_info> users_table;
     users_table _users;
-    
-    TABLE game_struct {
-      uint64_t gameid = 0;
-      bool isenabled;
-      std::string gameName;
-        
-    };
-     
-    typedef eosio::multi_index
-    <name("games"), game_struct> game_table;
-    game_table _games;
+
 };
 
-EOSIO_DISPATCH(cardgame, (hi))
+EOSIO_DISPATCH(cardgame, (login))
--- cardgame/cardgame/src/cardgame.cpp
+++ cardgame/cardgame/src/cardgame.cpp
@@ -1,15 +1,19 @@
 #include <cardgame.hpp>
 #include "gameplay.cpp"
 
-ACTION cardgame::login(name user) {
+ACTION cardgame::login(name user, std::string usermessage) {
   require_auth(user);
   //look through the _sers table to find user
   auto user_iterator = _users.find(user.value);
   
   //if user does not exist,
   if (user_iterator == _users.end()) {
-    
+     //create a new record for the user (_users)
+     user_iterator = _users.emplace
+      (user,[&](auto& new_user){
+        new_user.username = user;
+        new_user.usermessage = usermessage;
+      });
   }
-  //create a new record for the user (_users)
  
 }
