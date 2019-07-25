ce350e817ea9d6dab91874f9b5689c766820af69
mr-fool <mr-fool@users.noreply.github.com>
2019-07-24 18:19:31 -0600

    got it to work on window

--- cardgame/cardgame/include/cardgame.hpp
+++ cardgame/cardgame/include/cardgame.hpp
@@ -1,13 +1,45 @@
-#include <eosio/eosio.hpp>
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/print.hpp>
 
-using namespace std;
 using namespace eosio;
-CONTRACT cardgame : public eosio::contract {
 
+CONTRACT cardgame : public contract {
   public:
-
-    cardgame( name receiver, name code, datastream<const char*> ds ):
-      contract(receiver, code, ds)
+    using contract::contract;
+    cardgame(eosio::name receiver, 
+      eosio::name code, datastream<const char*> ds):
+      contract(receiver, code, ds), 
+      _users(receiver, receiver.value),
+      _games(receiver, receiver.value)
       {}
 
+    ACTION hi(name user);
+  
+  private:
+    TABLE user_info {
+      name username;
+      uint64_t win_count = 0;
+      uint64_t lost_count = 0;
+      
+      auto primary_key() const {
+        return username.value;
+        
+      }
+    };
+    typedef eosio::multi_index
+    <name("table"), user_info> users_table;
+    users_table _users;
+    
+    TABLE game_struct {
+      uint64_t gameid = 0;
+      bool isenabled;
+      std::string gameName;
+        
+    };
+     
+    typedef eosio::multi_index
+    <name("games"), game_struct> game_table;
+    game_table _games;
 };
+
+EOSIO_DISPATCH(cardgame, (hi))
--- cardgame/cardgame/src/cardgame.cpp
+++ cardgame/cardgame/src/cardgame.cpp
@@ -1,3 +1,12 @@
+#include <cardgame.hpp>
 #include "gameplay.cpp"
 
-EOSIO_DISPATCH(cardgame, BOOST_PP_SEQ_NIL)
+ACTION cardgame::hi(name user) {
+  require_auth(get_self());
+  print("Hello, ", name{user});
+}
+ACTION trymessage (name sender, name recipient) {
+    require_auth(sender);
+    print("Sorry, you will have to say hello to ", name{recipient}, " in person");
+ 
+}
