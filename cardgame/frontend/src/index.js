ca1fabd465a9ceddcecc8efbe05fd5cd4a22ba76
mr-fool <mr-fool@users.noreply.github.com>
2019-07-29 14:58:11 -0600

    Update index.js

--- cardgame/frontend/src/index.js
+++ cardgame/frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './components';
+import {Provider} from "react-redux";
+import store from './store';
 
 ReactDOM.render(
   <App />,
