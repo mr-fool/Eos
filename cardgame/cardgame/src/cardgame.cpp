1f3af438b4cecdb40d9859c3f1fc3c2403969dae
mr-fool <mr-fool@users.noreply.github.com>
2019-09-07 23:51:31 -0600

    Update cardgame.cpp

--- cardgame/cardgame/src/cardgame.cpp
+++ cardgame/cardgame/src/cardgame.cpp
@@ -16,7 +16,15 @@ ACTION cardgame::login(name username) {
 ACTION cardgame::startgame(name username) {
   require_auth(username);
   auto& user_data = _user.get(user.value, "User doesn't exist");
-  _users.modify(user_data, username, [&](row)) {
-    row.variable = 13;
+  _users.modify(user_data, username, [&](automodified_user_data)) {
+    game game_data;
+    
+    //Draw 4 cards for the player from their desks
+    for (uint8_t cards_drawn = 0; cards_drawn < 4 ; cards_drawn++) {
+      drawOne_card(game_data.deck_player, game_data.hand_player);
+      drawOne_card(game_data.deck_ai,game_data.hand_ai);
+    }
+    //Assign this game we just created to the user
+    modified_user_data.game_data = game_data;
   }
 }
