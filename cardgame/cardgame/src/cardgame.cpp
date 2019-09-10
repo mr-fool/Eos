3f1bbdb0ccbc78e10e1008e852f2c3c06e92d2f2
mr-fool <mr-fool@users.noreply.github.com>
2019-09-10 17:16:32 -0600

    fixed

--- cardgame/cardgame/src/cardgame.cpp
+++ cardgame/cardgame/src/cardgame.cpp
@@ -1,30 +1,35 @@
+#include "cardgame.hpp"
 #include "gameplay.cpp"
 
 ACTION cardgame::login(name username) {
-  // Ensure this action is authorized by the player
   require_auth(username);
   
-  // Create a record in the table if the player doesn't exist in our app yet
   auto user_iterator = _users.find(username.value);
   if (user_iterator == _users.end()) {
-    user_iterator = _users.emplace(username,  [&](auto& new_user) {
-      new_user.username = username;
-    });
-  } 
+    // create a new record for the user (_users)
+    user_iterator = _users.emplace
+      (username, [&](auto& new_user) {
+        new_user.username = username;
+      });
+  }
 }
 
 ACTION cardgame::startgame(name username) {
   require_auth(username);
-  auto& user_data = _user.get(user.value, "User doesn't exist");
-  _users.modify(user_data, username, [&](automodified_user_data)) {
+  
+  auto& user_data = _users.get(username.value, "User doesn't exist");
+  
+  _users.modify(user_data, username, [&](auto& modified_user_data) {
     game game_data;
     
-    //Draw 4 cards for the player from their desks
-    for (uint8_t cards_drawn = 0; cards_drawn < 4 ; cards_drawn++) {
-      drawOne_card(game_data.deck_player, game_data.hand_player);
-      drawOne_card(game_data.deck_ai,game_data.hand_ai);
+    //Draw 4 cards for the player and the AI from their decks
+    for (uint8_t cards_drawn = 0; cards_drawn < 4; cards_drawn++) {
+      draw_one_card(game_data.deck_player, game_data.hand_player);
+      draw_one_card(game_data.deck_ai, game_data.hand_ai);
     }
-    //Assign this game we just created to the user
+    
+    //assign this game we just created to the user
     modified_user_data.game_data = game_data;
-  }
+  });
+  
 }
--- cardgame/cardgame/src/gameplay.cpp
+++ cardgame/cardgame/src/gameplay.cpp
@@ -1,1 +1,51 @@
-#include "cardgame.hpp"
+#include <eosio/system.hpp>
+
+int cardgame::random(const int range) {
+  // find the existing seed 
+  auto seed_iterator = _seed.begin();
+  // initialize the seed if not found (if not used)
+  if (seed_iterator == _seed.end()) {
+    seed_iterator = _seed.emplace(_self, [&](auto& seed) {
+      // defaults are good enough
+    });
+  }
+  
+  int prime = 65337;
+  auto new_seed_value = (seed_iterator->value + eosio::current_time_point().sec_since_epoch()) % prime;
+  
+  _seed.modify(seed_iterator,_self,[&](auto& seed){
+    seed.value = new_seed_value;
+  });
+  
+  int random_result = new_seed_value % range;
+  return random_result;
+}
+
+void cardgame::draw_one_card
+      (vector<uint8_t>& deck, vector<uint8_t>& hand) {
+  // pick a random card from the deck
+  int deck_card_idx = random(deck.size());
+  
+  //find the first empty slot in the player's hand
+  int first_empty_slot = -1;
+  for (int slot_to_check = 0; slot_to_check <= hand.size();
+        slot_to_check++) {
+    auto id = hand[slot_to_check];
+    
+    if (card_dict.at(id).type == EMPTY) {
+      first_empty_slot = slot_to_check;
+      break;
+    }
+  }
+  
+check(first_empty_slot != -1,
+        "No empty slot in player's hand");
+        
+  // assign the card we randomly picked to the empty spot
+  hand[first_empty_slot] = deck[deck_card_idx];
+  
+  // erase the card from the player's/AI's deck
+  deck.erase(deck.begin() + deck_card_idx);
+  
+  //place the card in that slot
+}
