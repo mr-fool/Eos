75e9c81b60fbf695cc60495873365f712c320429
mr-fool <mr-fool@users.noreply.github.com>
2019-09-13 17:25:36 -0600

    Update cardgame.cpp

--- cardgame/cardgame/src/cardgame.cpp
+++ cardgame/cardgame/src/cardgame.cpp
@@ -34,7 +34,20 @@ ACTION cardgame::startgame(name username) {
   
 }
 
-ACTION cardgame::playcard(name username,uint8_t player_card_idx) {
+ACTION cardgame::playcard(name username,uint8_t player_card_hand_idx) {
   
   //Ensure that the player has authorize this cardgame 
+  require_auth(username);
+  
+  //Check that the card has been selected is valid
+  eosio_assert(player_card_hand_idx < 4, "playcard: invalid hand index");
+  auto& user_data = _users.get(username.value, "User doesn't exist");
+  
+  //Verify that the game status is suitable
+  eosio_assert(user_data.game_data == ONGOING, "playcard: This game has already ended");
+  eosio_assert(user_data.game_data.selected_card_player == 0, "playcard: Player has already played a card this turn!");
+  
+  //Assign the selected card from the player's hand
+  
+  
 }
